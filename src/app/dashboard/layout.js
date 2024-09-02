6ff0d1de63bb709bae4aa130ae1843b9c8cc6b26
adenikinju daniel <adenikinjudaniel@gmail.com>
2024-09-01 22:59:59 -0400

    mobile issue resolution

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function layout({ children }) {
   return (
-    <div className="h-screen bg-beige_100">
+    <div className="h-screen relative bg-beige_100">
       <div className="sm:flex">
         <Navbar />
         <div className="sm:ml-[15%] w-full h-full">{children}</div>
